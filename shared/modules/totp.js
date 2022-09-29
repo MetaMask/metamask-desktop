1b2913d13bc811ee89f76460f700460a5160863d
Vinicius Stevam <vinicius.stevam@consensys.net>
2022-09-29 20:34:01 +0100

    add totp library

---
+++ shared/modules/totp.js
@@ -0,0 +1,33 @@
+import createId from './random-id';
+import * as OTPAuth from 'otpauth';
+
+let totp;
+
+const newTOTP = () => {
+  const secretKey = createId()
+  return new OTPAuth.TOTP({
+    issuer: 'MM',
+    label: 'MetaMask',
+    algorithm: 'SHA1',
+    digits: 6,
+    period: 30,
+    secret: secretKey
+  });
+};
+
+export const generate = () => {
+  if (!totp) {
+    totp = newTOTP();
+  }
+  return totp.generate();
+};
+
+export const validate = (token) => {
+  if (!totp) {
+    totp = newTOTP();
+  }
+  return totp.validate({
+    token,
+    window: 1,
+  });
+};
