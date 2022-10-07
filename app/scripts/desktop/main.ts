daf050e39e04434aa1bc41727eb91e56f7d9c01e
cryptotavares <joao.tavares@consensys.net>
2022-10-07 11:49:36 +0100

    feat: add lavamoat runtime for dev mode
    Enable lavamoat runtime for desktop app
    dev mode (not packaged apps)

---
+++ app/scripts/desktop/main.ts
@@ -0,0 +1,32 @@
+import path from 'path';
+import { app } from 'electron';
+import { runLava } from 'lavamoat';
+
+let appPath = process.cwd();
+let policyRelativePath = '/lavamoat/desktop/node/';
+
+if (app.isPackaged) {
+  appPath = app.getAppPath();
+  policyRelativePath = '/lavamoat/desktop/electron/';
+}
+
+const entryPath = path.join(appPath, '/dist_desktop/app/scripts/background.js');
+const policyPath = path.join(appPath, policyRelativePath, 'policy.json');
+const policyOverridePath = path.join(
+  appPath,
+  policyRelativePath,
+  'policy-override.json',
+);
+
+runLava({
+  // eslint-disable-next-line @typescript-eslint/no-require-imports
+  entryPath,
+  policyPath,
+  policyOverridePath,
+  projectRoot: appPath,
+  isPackagedApp: app.isPackaged,
+}).catch((err: Error) => {
+  // explicity log stack to workaround https://github.com/endojs/endo/issues/944
+  console.error(err.stack || err);
+  process.exit(1);
+});
