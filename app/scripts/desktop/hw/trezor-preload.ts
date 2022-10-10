7fc8708b3483112188bc7e768d395b92682614e8
Bernardo Garces Chapero <bernardo.chapero@consensys.net>
2022-10-10 15:37:05 +0100

    add types

--- app/scripts/desktop/hw/trezor-preload.ts
+++ app/scripts/desktop/hw/trezor-preload.ts
@@ -1,17 +1,19 @@
 import { ipcRenderer } from 'electron';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import TrezorConnectType from 'trezor-connect';
 
 // TrezorConnect is injected as inline script in html
 // therefore it doesn't need to included into node_modules
 // get reference straight from window object
-let TrezorConnect: any;
+let TrezorConnect: typeof TrezorConnectType;
 
 const onLoad = () => {
   ipcRenderer.on('trezor-connect-init', (_, payload) => {
-    TrezorConnect.on('DEVICE_EVENT', (deviceEvent: any) => {
+    TrezorConnect.on('DEVICE_EVENT', (deviceEvent) => {
       ipcRenderer.send('trezor-connect-on-device-event', deviceEvent);
     });
 
-    TrezorConnect.init(payload).then((response: any) => {
+    TrezorConnect.init(payload).then((response) => {
       ipcRenderer.send('trezor-connect-init-result', response);
     });
   });
@@ -21,13 +23,13 @@ const onLoad = () => {
   });
 
   ipcRenderer.on('trezor-connect-getPublicKey', (_, payload) => {
-    TrezorConnect.getPublicKey(payload).then((response: any) => {
+    TrezorConnect.getPublicKey(payload).then((response) => {
       ipcRenderer.send('trezor-connect-getPublicKey-result', response);
     });
   });
 
   ipcRenderer.on('trezor-connect-ethereumSignTransaction', (_, payload) => {
-    TrezorConnect.ethereumSignTransaction(payload).then((response: any) => {
+    TrezorConnect.ethereumSignTransaction(payload).then((response) => {
       ipcRenderer.send(
         'trezor-connect-ethereumSignTransaction-result',
         response,
@@ -36,13 +38,13 @@ const onLoad = () => {
   });
 
   ipcRenderer.on('trezor-connect-ethereumSignMessage', (_, payload) => {
-    TrezorConnect.ethereumSignMessage(payload).then((response: any) => {
+    TrezorConnect.ethereumSignMessage(payload).then((response) => {
       ipcRenderer.send('trezor-connect-ethereumSignMessage-result', response);
     });
   });
 
   ipcRenderer.on('trezor-connect-ethereumSignTypedData', (_, payload) => {
-    TrezorConnect.ethereumSignTypedData(payload).then((response: any) => {
+    TrezorConnect.ethereumSignTypedData(payload).then((response) => {
       ipcRenderer.send('trezor-connect-ethereumSignTypedData-result', response);
     });
   });
