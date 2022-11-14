3c09b86ee2d55e04ffe95fd6e21681faf38204ed
Matthew Walsh <matthew.walsh@consensys.net>
2022-11-14 21:00:13 +0000

    Generate declaration files for common package
    Add dependency to common CI jobs.

--- packages/app/app/scripts/desktop/test/utils.ts
+++ packages/app/app/scripts/desktop/test/utils.ts
@@ -1,5 +1,5 @@
 import { Duplex, EventEmitter } from 'stream';
-import { BrowserWebSocket, NodeWebSocket } from '../shared/web-socket-stream';
+import { BrowserWebSocket, NodeWebSocket } from '@metamask/desktop';
 
 export const flushPromises = (): Promise<void> =>
   new Promise((resolve) => setImmediate(resolve));
