b825ceb51b7bf994171d35f44b8493fecb192694
João Tavares <joao.tavares@consensys.net>
2022-11-16 12:47:30 +0000

    Add lavamoat runtime desktop app (#71)
    Enable lavamoat runtime for desktop app
    
    * fix: add lavamoat.d.ts types file
    
    * feat: add lavamoat support for packaged app
    
    * chore: exclude builds_desktop from tsc config

---
+++ app/scripts/desktop/main.ts
@@ -0,0 +1,31 @@
+import path from 'path';
+import { app } from 'electron';
+import { runLava } from 'lavamoat';
+
+let appPath = process.cwd();
+const policyRelativePath = '/lavamoat/desktop/node/';
+
+if (app.isPackaged) {
+  appPath = app.getAppPath();
+}
+
+const entryPath = path.join(appPath, '/dist_desktop/app/scripts/background.js');
+const policyPath = path.join(appPath, policyRelativePath, 'policy.json');
+const policyOverridePath = path.join(
+  appPath,
+  policyRelativePath,
+  'policy-override.json',
+);
+
+runLava({
+  // eslint-disable-next-line @typescript-eslint/no-require-imports
+  entryPath,
+  policyPath,
+  policyOverridePath,
+  projectRoot: appPath,
+  includeDevDeps: false,
+}).catch((err: Error) => {
+  // explicity log stack to workaround https://github.com/endojs/endo/issues/944
+  console.error(err.stack || err);
+  process.exit(1);
+});
