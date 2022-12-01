dcfb9f91633a6f7195caf894f9adeb78f48b0e62
Vinicius Stevam <vinicius.stevam@consensys.net>
2022-12-01 13:45:55 +0000

    removed ENABLE_MV3

--- packages/app/submodules/extension/shared/modules/mv3.utils.js
+++ packages/app/submodules/extension/shared/modules/mv3.utils.js
@@ -1,6 +1,4 @@
 import { browser } from '@metamask/desktop/dist/browser';
 
-const configOverride = false;
-
 export const isManifestV3 =
-  browser?.runtime.getManifest().manifest_version === 3 || configOverride;
+  browser?.runtime.getManifest().manifest_version === 3;
