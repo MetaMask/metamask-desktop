499548b56462327d30486ac67dda18fe92b1310d
Matthew Walsh <matthew.walsh@consensys.net>
2022-12-13 18:41:00 +0000

    chore: use separate cache for extension deps

--- scripts/init-submodule.sh
+++ scripts/init-submodule.sh
@@ -3,8 +3,13 @@
 # Exit immediately if a command exits with a non-zero status.
 set -e
 
+# Initialise and clone submodules
 git submodule init
 git submodule update
+
+# Write submodule commit hash to file for use by pipeline
+SUBMODULE_COMMIT=$(cd packages/app/submodules/extension && git rev-parse HEAD)
+echo $SUBMODULE_COMMIT > .submodule_commit 
 
 # Copy common package to submodule
 # Not required when package is published
