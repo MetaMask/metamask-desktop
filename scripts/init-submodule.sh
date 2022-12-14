72ac2dfcbfe8ca1e25321ba5ce69a2ee4afc3582
Matthew Walsh <matthew.walsh@consensys.net>
2022-12-14 10:54:15 +0000

    chore: cache extension deps using extension yarn lock
    Update lavamoat policy.

--- scripts/init-submodule.sh
+++ scripts/init-submodule.sh
@@ -6,7 +6,3 @@ set -e
 # Initialise and clone submodules
 git submodule init
 git submodule update
-
-# Write submodule commit hash to file for use by pipeline
-SUBMODULE_COMMIT=$(cd packages/app/submodules/extension && git rev-parse HEAD)
-echo $SUBMODULE_COMMIT > .submodule_commit
