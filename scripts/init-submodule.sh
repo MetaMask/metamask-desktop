27cccea8a98c57e15d97504804f234eb3b09391d
Vinicius Stevam <vinicius.stevam@consensys.net>
2023-01-04 14:00:20 +0000

    fix: host file null and disable host when ci true

--- scripts/init-submodule.sh
+++ scripts/init-submodule.sh
@@ -3,6 +3,13 @@
 # Exit immediately if a command exits with a non-zero status.
 set -e
 
+# Point the host file to null and disable host checking
+if [ "$CI" = "true" ];
+then
+    echo "CI: setting GIT_SSH_COMMAND"
+    export GIT_SSH_COMMAND="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
+fi
+
 # Initialise and clone submodules
 git submodule init
 git submodule update
