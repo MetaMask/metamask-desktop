6e7ddf4b3f91039154c99f8f8ee722f377fe89d3
cryptotavares <joao.tavares@consensys.net>
2023-01-16 21:18:21 +0000

    chore: use notarytool for notarization
    Use the notarytool instead of the legacy one.
    It is significantly faster and is the one
    recommended by apple

--- packages/app/build/mac/notarize.js
+++ packages/app/build/mac/notarize.js
@@ -1,38 +1,5 @@
 /* eslint-disable node/no-process-env, node/no-unpublished-require, jsdoc/require-jsdoc */
-const path = require('path');
-const { readFile } = require('fs/promises');
-const ini = require('ini');
 const { notarize } = require('electron-notarize');
-
-const configurationPropertyNames = ['APPLEID', 'APPLEIDPASS'];
-
-async function getConfig() {
-  const configPath = path.resolve(__dirname, '.apprc');
-  let configContents = '';
-  try {
-    configContents = await readFile(configPath, {
-      encoding: 'utf8',
-    });
-  } catch (error) {
-    if (error.code !== 'ENOENT') {
-      throw error;
-    }
-  }
-
-  const environmentVariables = {};
-  for (const propertyName of configurationPropertyNames) {
-    if (process.env[propertyName]) {
-      environmentVariables[propertyName] = process.env[propertyName];
-    }
-  }
-
-  return {
-    APPLE_ID: process.env.APPLE_ID,
-    APPLE_ID_PASS: process.env.APPLE_ID_PASS,
-    APP_BUNDLE_ID: process.env.APP_BUNDLE_ID,
-    ...ini.parse(configContents),
-  };
-}
 
 module.exports = async function notarizing(context) {
   const { electronPlatformName, appOutDir } = context;
@@ -42,12 +9,11 @@ module.exports = async function notarizing(context) {
 
   const appName = context.packager.appInfo.productFilename;
 
-  const config = await getConfig();
-
   await notarize({
-    appBundleId: config.APP_BUNDLE_ID,
+    tool: 'notarytool',
+    teamId: process.env.APPLE_TEAM_ID,
     appPath: `${appOutDir}/${appName}.app`,
-    appleId: config.APPLE_ID,
-    appleIdPassword: config.APPLE_ID_PASS,
+    appleId: process.env.APPLE_ID,
+    appleIdPassword: process.env.APPLE_ID_PASSWORD,
   });
 };
