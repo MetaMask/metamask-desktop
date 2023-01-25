e52e3884abeb291e707226b2a7445c51b9222e04
Bernardo Garces Chapero <bernardo.chapero@consensys.net>
2023-01-25 15:03:04 +0000

    fix: issue referencing submodule

--- packages/app/src/app/renderer/sentry-install.ts
+++ packages/app/src/app/renderer/sentry-install.ts
@@ -1,7 +1,6 @@
 import * as Sentry from '@sentry/electron/renderer';
 import { Dedupe, ExtraErrorData } from '@sentry/integrations';
 import { Integration } from '@sentry/types/dist/integration';
-import { FilterEvents } from 'submodules/extension/app/scripts/lib/sentry-filter-events';
 import { getSentryDefaultOptions } from './setup-sentry';
 
 declare const global: typeof globalThis & {
@@ -14,20 +13,9 @@ const sentryDefaultOptions = getSentryDefaultOptions(
 Sentry.init({
   ...sentryDefaultOptions,
   integrations: [
-    new FilterEvents({
-      getMetaMetricsEnabled: () => {
-        // TODO
-        // We are not currently sending metrics through this Sentry instance
-        // Add filter when we do
-        const desktopMetaMetricsOptIn = false;
-
-        electronLog.debug('Sentry metric filter for renderer process', {
-          desktopMetaMetricsOptIn,
-        });
-
-        return desktopMetaMetricsOptIn;
-      },
-    }),
+    // TODO
+    // We are not currently sending metrics through this Sentry instance
+    // Add filter when we do
     new Dedupe() as Integration,
     new ExtraErrorData() as Integration,
   ],
