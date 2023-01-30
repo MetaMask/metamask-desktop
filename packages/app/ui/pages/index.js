1325cc54c24656782141dce3b4982f320e6492b7
OGPoyraz <omergoktugpoyraz@gmail.com>
2023-01-30 10:37:10 +0100

    feat: implement error boundary to ui (#458)

--- packages/app/ui/pages/index.js
+++ packages/app/ui/pages/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import { Provider } from 'react-redux';
 import { HashRouter } from 'react-router-dom';
 import { PersistGate } from 'redux-persist/integration/react';
@@ -6,20 +6,47 @@ import PropTypes from 'prop-types';
 
 import { I18nProvider } from '../contexts/i18n';
 import Routes from './routes';
+import CriticalError from './error/critical-error.component';
 
-const Root = ({ store, persistor }) => {
-  return (
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <HashRouter>
+class Root extends PureComponent {
+  state = {};
+
+  static getDerivedStateFromError(error) {
+    return { error };
+  }
+
+  componentDidCatch(error) {
+    // TODO: Sentry.captureException(error);
+    console.error('Exception Error Page', error);
+  }
+
+  render() {
+    const { store, persistor } = this.props;
+    const { error } = this.state;
+
+    if (error) {
+      return (
+        <Provider store={store}>
           <I18nProvider>
-            <Routes />
+            <CriticalError error={error} />
           </I18nProvider>
-        </HashRouter>
-      </PersistGate>
-    </Provider>
-  );
-};
+        </Provider>
+      );
+    }
+
+    return (
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <HashRouter>
+            <I18nProvider>
+              <Routes />
+            </I18nProvider>
+          </HashRouter>
+        </PersistGate>
+      </Provider>
+    );
+  }
+}
 
 Root.propTypes = {
   /**
