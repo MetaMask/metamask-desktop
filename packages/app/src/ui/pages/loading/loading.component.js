b008298ae235c3527f2095c830aeadae72f1592e
cryptotavares <joao.tavares@consensys.net>
2023-02-17 18:14:28 +0000

    feat: close popup when no pending actions
    Replicate the behaviour from
    home component on the extension.
    Confirmation and permission pages
    route to the '/' (home) page. And
    then the home page decides if the
    notification popup window should be
    closed or not.

--- packages/app/src/ui/pages/loading/loading.component.js
+++ packages/app/src/ui/pages/loading/loading.component.js
@@ -1,7 +1,39 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import LoadingScreen from '../../../../submodules/extension/ui/components/ui/loading-screen';
 
-const Loading = () => {
+const Loading = ({
+  isNotification,
+  totalUnapprovedCount,
+  isSigningQRHardwareTransaction,
+  closeNotificationPopup
+}) => {
+  function shouldCloseNotificationPopup({
+    isNotification,
+    totalUnapprovedCount,
+    isSigningQRHardwareTransaction,
+  }) {
+    return (
+      isNotification &&
+      totalUnapprovedCount === 0 &&
+      !isSigningQRHardwareTransaction
+    );
+  }
+
+  useEffect(() => {
+    if (shouldCloseNotificationPopup({
+      isNotification,
+      totalUnapprovedCount,
+      isSigningQRHardwareTransaction
+    })) {
+      closeNotificationPopup();
+    }
+  }, [
+    isNotification,
+    totalUnapprovedCount,
+    isSigningQRHardwareTransaction,
+  ])
+
+
   return <LoadingScreen showLoadingSpinner />;
 };
 
