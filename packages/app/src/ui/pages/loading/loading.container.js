288debed8d8c8b866500ed4e0901e6b573f393a9
cryptotavares <joao.tavares@consensys.net>
2023-02-17 18:14:28 +0000

    feat: close popup when no pending actions
    Replicate the behaviour from
    home component on the extension.
    Confirmation and permission pages
    route to the '/' (home) page. And
    then the home page decides if the
    notification popup window should be
    closed or not.

--- packages/app/src/ui/pages/loading/loading.container.js
+++ packages/app/src/ui/pages/loading/loading.container.js
@@ -2,9 +2,30 @@ import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import { compose } from 'redux';
 import Loading from './loading.component';
+import {
+  getTotalUnapprovedCount,
+  hasUnsignedQRHardwareTransaction,
+  hasUnsignedQRHardwareMessage
+} from '../../../../submodules/extension/ui/selectors'
+import { closeNotificationPopup } from '../../../../submodules/extension/ui/store/actions'
 
-function mapStateToProps(_state) {
-  return {};
+function mapStateToProps(state) {
+  const totalUnapprovedCount = getTotalUnapprovedCount(state);
+  const isSigningQRHardwareTransaction =
+    hasUnsignedQRHardwareTransaction(state) ||
+    hasUnsignedQRHardwareMessage(state);
+
+  return {
+    isNotification: true,
+    totalUnapprovedCount,
+    isSigningQRHardwareTransaction
+  };
 }
 
-export default compose(withRouter, connect(mapStateToProps))(Loading);
+function mapDispatchToProps() {
+  return {
+    closeNotificationPopup: () => closeNotificationPopup(),
+  };
+}
+
+export default compose(withRouter, connect(mapStateToProps, mapDispatchToProps))(Loading);
