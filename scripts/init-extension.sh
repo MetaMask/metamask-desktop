4577a018925ac16154262fd121208638939530b5
cryptotavares <joao.tavares@consensys.net>
2023-02-24 14:41:03 +0000

    fix: e2e tests

--- scripts/init-extension.sh
+++ scripts/init-extension.sh
@@ -7,6 +7,22 @@ if [ ! "$NO_COMMON_LINK" ]; then
   cd packages/app/submodules/extension
   yarn link ../../../common
   cd -
+
+  # Ensure the React dependencies are identical in the extension and app to avoid two React instances at runtime in app UI
+  echo "Linking extension dependencies to app"
+  for DEPENDENCY in $(ls $EXTENSION_DIR/node_modules | grep react); do
+    rm -rf $EXTENSION_DIR/node_modules/$DEPENDENCY
+    ln -s ../../../node_modules/$DEPENDENCY/ $EXTENSION_DIR/node_modules/$DEPENDENCY
+    echo "Processed extension dependency: $DEPENDENCY"
+  done
+
+  # Extend the webextension-polyfill patch to Extension deps
+  echo "Linking extension dependencies to app"
+  for DEPENDENCY in $(ls $EXTENSION_DIR/node_modules | grep webextension-polyfill); do
+    rm -rf $EXTENSION_DIR/node_modules/$DEPENDENCY
+    ln -s ../../../node_modules/$DEPENDENCY/ $EXTENSION_DIR/node_modules/$DEPENDENCY
+    echo "Processed extension dependency: $DEPENDENCY"
+  done
 fi
 
 # Ensure an extension env file exists
@@ -23,18 +39,3 @@ if [ ! -f "packages/app/.env" ]; then
   cp packages/app/.env.example packages/app/.env
 fi
 
-# Ensure the React dependencies are identical in the extension and app to avoid two React instances at runtime in app UI
-echo "Linking extension dependencies to app"
-for DEPENDENCY in $(ls $EXTENSION_DIR/node_modules | grep react); do
-  rm -rf $EXTENSION_DIR/node_modules/$DEPENDENCY
-  ln -s ../../../node_modules/$DEPENDENCY/ $EXTENSION_DIR/node_modules/$DEPENDENCY
-  echo "Processed extension dependency: $DEPENDENCY"
-done
-
-# Extend the webextension-polyfill patch to Extension deps
-echo "Linking extension dependencies to app"
-for DEPENDENCY in $(ls $EXTENSION_DIR/node_modules | grep webextension-polyfill); do
-  rm -rf $EXTENSION_DIR/node_modules/$DEPENDENCY
-  ln -s ../../../node_modules/$DEPENDENCY/ $EXTENSION_DIR/node_modules/$DEPENDENCY
-  echo "Processed extension dependency: $DEPENDENCY"
-done
