b44e26bf2991f2853ed3e0fba532a6d29b08ccb6
Vinicius Stevam <45455812+vinistevam@users.noreply.github.com>
2023-02-26 05:59:15 +0000

    fix: add graceful shutdown for sentry to flush all pending messages (#536)
    * fix: add metrics decision filter to the renderer

--- packages/app/src/app/globals.ts
+++ packages/app/src/app/globals.ts
@@ -98,16 +98,10 @@ if (!global.self) {
           const extensionMetaMetricsOptIn =
             extensionState.store?.metamask?.participateInMetaMetrics;
 
-          const desktopMetaMetricsOptInValue = readPersistedSettingFromAppState(
-            {
-              defaultValue: false,
-              key: 'metametricsOptIn',
-            },
-          );
-          const desktopMetaMetricsOptIn =
-            typeof desktopMetaMetricsOptInValue === 'string'
-              ? desktopMetaMetricsOptInValue === 'true'
-              : desktopMetaMetricsOptInValue;
+          const desktopMetaMetricsOptIn = readPersistedSettingFromAppState({
+            defaultValue: false,
+            key: 'metametricsOptIn',
+          });
 
           // Desktop opt in must be enabled
           // Extension opt in must be enabled if desktop currently enabled
